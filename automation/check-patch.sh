a6171218a8286255f78812d18ed4ffc9e641aa3f
Ales Musil <amusil@redhat.com>
2022-01-05 10:34:52 +0100

    automation: Move automation to GH actions
    Add GH actions for rpm build.

--- automation/check-patch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-# todo: any other test
-
-# test building
-./automation/build-artifacts.sh
---
+++ automation/rpm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+EXPORT_DIR="${EXPORT_DIR:=exported-artifacts}"
+
+mkdir -p $EXPORT_DIR
+
+mkdir -p "`rpm --eval %_topdir`" "`rpm --eval %_sourcedir`"
+
+make rpm
+
+cp ovirt-lldp-labeler-*.tar.gz $EXPORT_DIR/
+
+cp ~/rpmbuild/RPMS/noarch/ovirt-lldp-labeler-*.noarch.rpm $EXPORT_DIR/
+cp ~/rpmbuild/SRPMS/ovirt-lldp-labeler-*.src.rpm $EXPORT_DIR/
+
+createrepo_c "$EXPORT_DIR"
